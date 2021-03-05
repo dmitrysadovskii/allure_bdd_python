33925c90b1a625c73b3abaafa46b8a589dcfb552
DSadovsky <DSadovsky@arlo.com>
2021-03-05 21:00:15 +0300

    fixed locator

--- locator/account_page_locator.py
+++ locator/account_page_locator.py
@@ -3,4 +3,4 @@ from selenium.webdriver.common.by import By
 
 class AccountPageLocator:
 
-    LOCATOR_MY_ACCOUNT_TEXT = (By.CLASS_NAME, 'row addresses-lists')
+    LOCATOR_MY_ACCOUNT_TEXT = (By.CLASS_NAME, '.row.addresses-lists')
