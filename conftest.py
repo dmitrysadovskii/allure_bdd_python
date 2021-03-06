bede641d55aa08fcccd23671b7d2bab7932a57c0
DSadovsky <DSadovsky@arlo.com>
2021-03-06 12:36:04 +0300

    add headless tests

--- conftest.py
+++ conftest.py
@@ -13,7 +13,12 @@ def load_config(file_path):
 
 @pytest.fixture()
 def browser(request):
-    driver = webdriver.Chrome()
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-gpu')
+    driver = webdriver.Chrome(chrome_options=chrome_options)
+    # driver = webdriver.Chrome()
     driver.maximize_window()
     driver.get("http://automationpractice.com/index.php")
     failed_before = request.session.testsfailed
