909457748e938b3365b5d4b34a0aa58116bbc97f
dmitrysadovskii <48434081+dmitrysadovskii@users.noreply.github.com>
2021-09-20 00:48:06 +0300

    Update test_login_to_the_system.py

--- tests/test_login_to_the_system.py
+++ tests/test_login_to_the_system.py
@@ -18,7 +18,7 @@ def test_login_to_the_system(browser):
         AccountPage(browser).should_be_account_page()
         
 @allure.story("Check invalid user can login to the system")
-def test_login_to_the_system(browser):
+def test_invalid_login_to_the_system(browser):
     with allure.step('Open login page'):
         main_page = MainPage(browser)
         main_page.open_login_page()
