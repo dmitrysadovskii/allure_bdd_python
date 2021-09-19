8d2a21929765b3cd26df75d168a676262f1ee2f4
dmitrysadovskii <48434081+dmitrysadovskii@users.noreply.github.com>
2021-09-20 00:43:49 +0300

    Update test_login_to_the_system.py

--- tests/test_login_to_the_system.py
+++ tests/test_login_to_the_system.py
@@ -16,3 +16,16 @@ def test_login_to_the_system(browser):
         login_page.login("dmitry@gmail.com", "11111")
     with allure.step('Check account page displays'):
         AccountPage(browser).should_be_account_page()
+        
+@allure.story("Check invalid user can login to the system")
+def test_login_to_the_system(browser):
+    with allure.step('Open login page'):
+        main_page = MainPage(browser)
+        main_page.open_login_page()
+    with allure.step('Check login page displays'):
+        login_page = LoginPage(browser)
+        login_page.should_be_login_page()
+    with allure.step('Login as a user "dmitry@gmail.com"'):
+        login_page.login("dmitry@gmail.com", "111112")
+    with allure.step('Check account page displays'):
+        AccountPage(browser).should_be_account_page()
